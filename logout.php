5ac5d6298948d0c8b2ac58ada755a965b27675a5
chan <chan@semantia.com.au>
2020-10-15 19:05:22 +0800

    logout page

---
+++ logout.php
@@ -0,0 +1,11 @@
+<?php 
+header("Location: /MyFriendSystem/index.php");
+session_start();
+/**
+ * Status True = Login
+ * Status False = Logout
+*/
+$_SESSION["status"] = false;	
+$_SESSION["email"] = "";
+
+?>
